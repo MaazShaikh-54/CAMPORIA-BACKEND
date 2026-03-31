61b778117166c7100b87c9f3ab6b36ada297f026
Maaz <mohdmaaz.612054.it@mhssce.ac.in>
2026-04-01 01:09:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
+mongoose.set("strictQuery", true);
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDb connection error: ", err));
 
 app.use("/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server runnning on port ${PORT}`));
